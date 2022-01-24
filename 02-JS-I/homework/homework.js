e7088032fd68fa22caefb6112812fad9e5467dfe
Lio9708 <leonelagag@gmail.com>
2022-01-24 13:01:32 -0600

    push homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,7 +60,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-if (x=y) {return true;}
+if (x===y) {return true;}
   else return false;
 }
 
@@ -68,7 +68,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length = str2.length) {return true;}
+  if (str1.length === str2.length) {return true;}
     else return false;
 
 }
